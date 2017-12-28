a7657d8fb95e89fad85a13e9ae18169894e6da9c
Sang Putu Santika Wiranatha <34614430+wiranatha98@users.noreply.github.com>
2017-12-28 11:52:42 +0700

    versi 1.1

--- delete.php
+++ delete.php
@@ -63,6 +63,7 @@ while ($data = mysql_fetch_assoc($query)){
 	echo'<td> '.$data['umur'].'</td>';
 	echo'<td> '.$data['gender'].'</td>';
 	echo'<td> '.$data['kecamatan'].'</td>';
+	echo'<td> '.$data['jenis'].'</td>';
 	echo'<td> '.$data['keluhan'].'</td>';
 	echo'<td> '.$data['saran'].'</td>';
 	echo'<td> <a href="proses_delete.php?id='.$data['id'].'"> <right><i class="material-icons">delete</i></right> </td>';
