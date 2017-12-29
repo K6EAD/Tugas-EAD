6bfa1e2ccbcf0613747bd49764e8376c530d12e3
Isye S  Adhiwinaya <34766626+isyeadhiwi@users.noreply.github.com>
2017-12-29 16:13:55 +0700

    Update proses_inputsql.php

--- proses_inputsql.php
+++ proses_inputsql.php
@@ -11,13 +11,25 @@ $kecamatan = $_POST ['kecamatan'];
 $jenis = $_POST ['jenis'];
 $keluhan = $_POST ['keluhan'];
 $saran = $_POST ['saran'];
-$pict = $_POST ['pict'];
+$lokasi_file = $_FILES['gambar']['tmp_name'];
+$tipe_file   = $_FILES['gambar']['type'];
+$nama_file   = $_FILES['gambar']['name'];
+$direktori   = "upload/$nama_file";
 
-$input = mysql_query("INSERT INTO tb_database VALUES('','$nama','$nik','$umur','$gender','$kecamatan','$jenis','$keluhan','$saran','$pict')");
+if(!empty($lokasi_file)){
+	move_uploaded_file($lokasi_file,$direktori);
+  $input = mysql_query("INSERT INTO tb_database VALUES('','$nama','$nik','$umur','$gender','$kecamatan','$jenis','$keluhan','$saran','$nama_file')");
 
 if(!$input){
 echo "Tidak Tersambung ke Tabel";
+}else {
+	echo 'berhasil';
 }
+}else {
+	echo 'error';
+	
+}
+
 
 header('Location:insert.php');
 
